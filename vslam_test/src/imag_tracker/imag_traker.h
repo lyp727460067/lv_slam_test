8da63f065d143ecc1719ccdb5de3717f9b410b03
lyp727460067 <727460067@qq.com>
2021-01-29 19:27:06 +0800

    add  image traker

--- vslam_test/src/imag_tracker/imag_traker.h
+++ vslam_test/src/imag_tracker/imag_traker.h
@@ -2,14 +2,25 @@
 #define  _IMAG_TRAKER_H
 #include "Eigen/Core"
 #include "opencv2/opencv.hpp"
+
+class MapInterface {
+ public:
+  virtual std::unique_ptr<MapInterface> GetData() = 0;
+};
+
+class LocalMap : public MapInterface {};
+class GlobeMap : public MapInterface {};
+
 class ImagTraker
 {
   public:
   ImagTraker();
-  void AddImage(cv::Mat image);
-  bool LocalMatch(Eigen::Matrix4d &init_pose, 
-  
-  )
+  void PoseExtrapolator(Eigen::Matrix4d &init_pose, const cv::Mat &target,
+                        const cv::Mat &source, Eigen::Matrix4d *relate_pose);
+  void TrakeMap(const Eigen::Matrix4d &init_pose, const cv::Mat &source,
+               std::unique_ptr<MapInterface> map,Eigen::Matrix4d *relate_pose);
+
+ private:  
 
 
 
